f39b963cbc2105949a72b49fe281b948a538131a
yuka machida <sh0ge0405@gmail.com>
2025-02-24 13:05:38 +0900

    最初のコミット

---
+++ src/routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+//Route::post('register', [RegisteredUserController::class, 'store']);
+//Route::post('login', [AuthenticatedSessionController::class, 'store']);
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
