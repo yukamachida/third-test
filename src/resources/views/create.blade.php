830b0fdd9760d4f94eb8363d49f4e16819e59345
yuka machida <sh0ge0405@gmail.com>
2025-02-27 06:44:10 +0900

    初めてのコミット

---
+++ src/resources/views/create.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PiGLy</title>
+</head>
+<body>
+    体重登録画面
+</body>
+</html>
